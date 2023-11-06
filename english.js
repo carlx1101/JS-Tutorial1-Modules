97c790bbcb1595a189be6ba4457bd827078ef8c3
Carl <carlyip36@gmail.com>
2023-11-06 22:03:15 +0800

    Implemented modules concepts in index.js and english.js

---
+++ english.js
@@ -0,0 +1,7 @@
+function pluralize (word) { 
+    return word + 's';
+}
+
+const longestWord = 'loooooonggesttttword'
+
+module.exports = {pluralize, longestWord};
---
+++ index.js
@@ -0,0 +1,8 @@
+
+const {longestWord, pluralize} = require('./english.js')
+
+const pluralWord = english.pluralize('dog')
+const longestWord = english.longestWord
+
+console.log(pluralWord);
+console.log(longestWord);
